78b82e585375bd2f25a6b663d76ab72d53d1d538
koteshwar <janaganikoteshwargoud>
2025-08-24 11:38:28 +0530

    updated message

--- src/exam/SumOfTwoNumbers.java
+++ src/exam/SumOfTwoNumbers.java
@@ -1,9 +1,16 @@
-package exam;
+	SumOfTwoNumbers;	
+import java.util.Scanner;
+		class Sample {
+			void sum() {
+		 	Scanner sc = new Scanner(System.in);
+				int num1, num2, sum;
+				System.out.println("Enter two numbers:");
+				num1 = sc.nextInt();
+				num2 = sc.nextInt();
+				sum = num1 + num2;
+				System.out.println("Sum of two numbers=" + sum);
+			}
+		
+			}
 
-public class SumOfTwoNumbers {
 
-	public static void main(String[] args) {
-
-	}
-
-}
