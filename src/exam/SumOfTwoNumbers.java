f3fade30d6eac28ae84ed1220bceba2e76f4d5dc
deepthi <244g1a0577@srit.ac.in>
2025-08-24 11:48:08 +0530

    updated code

--- src/exam/SumOfTwoNumbers.java
+++ src/exam/SumOfTwoNumbers.java
@@ -1,9 +1,14 @@
 package exam;
-
+import java.util.*;
 public class SumOfTwoNumbers {
-
-	public static void main(String[] args) {
-
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+    int num1,num2,sum;
+    System.out.println("enter two number:");
+    num1=sc.nextInt();
+    num2=sc.nextInt();
+    sum=num1+num2;
+    System.out.println("Sum of two numbers:"+sum);
 	}
 
 }
